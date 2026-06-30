c9fe2ac306514d0b9548560ceaeb05256e22dfa8
Chase Dodge <75660257+Chas-w@users.noreply.github.com>
2026-06-30 07:41:42 -0400

    push for work

--- Scripts/IK_Controller.gd
+++ Scripts/IK_Controller.gd
@@ -1,18 +1,23 @@
 extends Node3D
+@export_category("Movement")
+@export var character_body : CharacterBody3D
+
+
 @export_category("Puppet Strings")
-@export var left_bicep : Node3D
-@export var left_forearm : Node3D
-@export var right_bicep : Node3D
-@export var right_forearm : Node3D
-@export var left_knee : Node3D
-@export var left_ankle : Node3D
-@export var right_knee : Node3D
-@export var right_ankle : Node3D
+@export var left_bicep : Marker3D
+@export var left_forearm : Marker3D
+@export var right_bicep : Marker3D
+@export var right_forearm : Marker3D
+@export var left_knee : Marker3D
+@export var left_ankle : Marker3D
+@export var right_knee : Marker3D
+@export var right_ankle : Marker3D
 
 @export_category("Control Categories")
-@export var hips : Node3D
-@export var torso : Node3D
-@export var shoulders : Node3D
+@export var all_limbs_and_checkers : Array[Marker3D]
+@export var hips : Marker3D
+@export var torso : Marker3D
+@export var shoulders : Marker3D
 
 @export_category("Movement Constraints")
  ##defaults to true, leads with left foot
@@ -24,6 +29,22 @@ var step_distance
 @export_range(1,100,.5) var fly_range_max : float 
 var fly_range
 
+@export_category("Raycasts")
+@export var step_left : RayCast3D
+@export var step_right : RayCast3D
+var ready_for_step : bool
+
 func _ready():
 	step_distance = step_distance_max
 	fly_range = fly_range_max
+	
+
+func _process(delta):
+	torso.global_position = character_body.global_position
+	torso.rotation = character_body.global_rotation
+
+func _step_checker():
+	if(step_left.get_collider().is_in_group("Ground")):
+		pass
+	else: 
+		return
